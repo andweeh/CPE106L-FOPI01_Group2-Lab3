752ca7ddcd668e029da4a1dae233260457ebdda5
Paul Jess Rosero <125846655+pauljessrosero@users.noreply.github.com>
2023-03-19 19:57:36 +0800

    sort method update

--- Problem 2.py
+++ Problem 2.py
@@ -1,12 +1,3 @@
-
-
- # Sort Method in ascending order
- #<-- after shuffle list method
-student_sort.sort()
-for students in students_sort:
+# Sort Method in ascending order
+students.sort()
 print(students)
-
-# Sort Method in descending order
-#<-- after shuffle list method
-  # student_sort.sort(reverse=True)
-  # print(students)
